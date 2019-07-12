1d6fec7cb237c35e4140629ce63de50b49ed3118
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-07-12 13:34:05 +0200

    Implement simple shortNode hashing

--- hash.go
+++ hash.go
@@ -1,9 +1,21 @@
 package proof
 
-import (
-	"github.com/ethereum/go-ethereum/common"
-)
+import "github.com/ethereum/go-ethereum/rlp"
 
-func hashShortNode(n *shortNode) common.Hash {
-	return common.Hash{}
+func hashShortNode(n *shortNode) []byte {
+	h := newHasher(0, 0, nil)
+	// h.tmp.Reset()
+
+	if _, ok := n.Val.(valueNode); !ok {
+		panic("only implemented for value nodes")
+	}
+
+	collapsed := n.copy()
+	collapsed.Key = hexToCompact(n.Key)
+
+	if err := rlp.Encode(&h.tmp, collapsed); err != nil {
+		panic("encode error: " + err.Error())
+	}
+	hash := h.makeHashNode(h.tmp)
+	return hash
 }
